93dfd79cf9cbd60973b2069761a006e0df9beb09
Ryo Hirano <ryo.hirano@monocrea.co.jp>
2021-04-24 14:48:24 +0900

    initial commit.

---
+++ src/test/java/jp/jwt/test/jwttest/JwtTestApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.jwt.test.jwttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
