636cce192148a87628fa06ab8089aa21bd3aa6c3
Ryo Hirano <ryo.hirano@monocrea.co.jp>
2021-05-01 11:45:23 +0900

    feat:fail test.

--- src/test/java/jp/jwt/test/jwttest/JwtTestApplicationTests.java
+++ src/test/java/jp/jwt/test/jwttest/JwtTestApplicationTests.java
@@ -1,5 +1,6 @@
 package jp.jwt.test.jwttest;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.nio.charset.StandardCharsets;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+
 import io.jsonwebtoken.security.Keys;
 
 @SpringBootTest
@@ -25,19 +27,37 @@ class JwtTestApplicationTests {
 	@BeforeEach
 	void createJws() {
 		secretKeyBytes = createSecretKeyBytes();
-		jws = Jwts.builder().setSubject(SUBJECT).signWith(Keys.hmacShaKeyFor(secretKeyBytes)).compact();
+		jws = Jwts.builder()
+				.setSubject(SUBJECT)
+				.signWith(Keys.hmacShaKeyFor(secretKeyBytes)).compact();
 	}
 
 	@Test
-	void jwtTest() {
-		Jws<Claims> parsedJws = Jwts.parserBuilder().setSigningKey(secretKeyBytes).build().parseClaimsJws(jws);
+	void jwtSuccess() {
+		Jws<Claims> parsedJws = Jwts.parserBuilder()
+									.setSigningKey(secretKeyBytes)
+									.build()
+									.parseClaimsJws(jws);
+		assertAll("success", 
+			() -> assertEquals(parsedJws.getBody().getSubject(), SUBJECT),
+			() -> assertEquals(parsedJws.getHeader().getAlgorithm(), AUTH_ALGORITHM));
+	}
 
-		assertEquals(parsedJws.getBody().getSubject(), SUBJECT);
-		assertEquals(parsedJws.getHeader().getAlgorithm(), AUTH_ALGORITHM);
+	@Test
+	void jwtFail() {
+		Jws<Claims> parsedJws = Jwts.parserBuilder()
+									.setSigningKey(secretKeyBytes)
+									.build()
+									.parseClaimsJws(jws);
+
+		assertAll("fail", 
+			() -> assertEquals(parsedJws.getBody().getSubject(),
+								SUBJECT.substring(0, SUBJECT.length() - 1)),
+			() -> assertEquals(parsedJws.getHeader().getAlgorithm(),
+								AUTH_ALGORITHM.substring(0, AUTH_ALGORITHM.length() -1)));
 	}
 
 	private byte[] createSecretKeyBytes() {
 		return UUID.randomUUID().toString().getBytes(StandardCharsets.UTF_8);
 	}
-
 }
